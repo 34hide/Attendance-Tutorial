5184b11ef88ebc7bded593100c77e4de4d5d266b
34hide <qqqp4ryd@able.ocn.ne.jp>
2020-11-29 05:21:31 +0000

    5.5.5ユーザーを保存しよう

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to @user
       # 保存に成功した場合は、ここに記述した処理が実行されます。
     else
       render :new
