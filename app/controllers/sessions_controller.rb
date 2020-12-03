3a5d2b93020bd29651bddefed092781ce78e1900
34hide <qqqp4ryd@able.ocn.ne.jp>
2020-12-03 12:42:16 +0000

    6.2.3エラーメッセージを表示しよう

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
 class SessionsController < ApplicationController
+
   def new
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ログイン後にユーザー情報ページにリダイレクトします。
     else
-      # ここにはエラーメッセージ用のflashを入れます。
+      flash.now[:danger] = '認証に失敗しました。'
       render :new
     end
   end
